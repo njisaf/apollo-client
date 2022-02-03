29139e90b9c5a14c80a48dd4d301ae9de6ca2daa
Nassir Isaf <nasisaf@WKMZTED39DD2.global.publicisgroupe.net>
2022-02-03 11:12:16 -0500

    Why not just use the apollo handler??

--- src/apollo-server.ts
+++ src/apollo-server.ts
@@ -1,44 +1,107 @@
-
-import { ApolloServer } from 'apollo-server-lambda';
+import {
+    ApolloServer
+} from "apollo-server-lambda";
 // import { DocumentClient } from 'aws-sdk/clients/dynamodb';
 
-import { resolvers } from './resolvers';
-import { typeDefs } from './type-defs';
+import {
+    resolvers
+} from "./resolvers";
+import {
+    typeDefs
+} from "./type-defs";
 
-import EstablishmentsDB from './datasources/establishmentsDB';
+import EstablishmentsDB from "./datasources/establishmentsDB";
 
-// const client: DocumentClient = new DocumentClient({
-//     apiVersion: 'latest',
-//     region: 'us-east-1',
-// });
-
+const establishmentPath = "/establishment";
 
 const apolloServer = new ApolloServer({
     resolvers,
     typeDefs,
-    context: ({event, context}) => {
-        console.log('serverless event: ', event);
-        console.log('serverless context: ', context);
+    context: ({
+        event,
+        context
+    }) => {
+        console.log("serverless event: ", event);
+        console.log("serverless context: ", context);
         return {
             headers: event.headers,
             functionName: context.functionName,
             event,
-            context
-        }
+            context,
+        };
     },
     dataSources: () => {
         return {
-            establishmentsDB: new EstablishmentsDB()
-        }
+            establishmentsDB: new EstablishmentsDB(),
+        };
     },
     introspection: true,
 });
 
-export const graphqlHandler = apolloServer.createHandler({
+const apolloHandler = apolloServer.createHandler({
     expressGetMiddlewareOptions: {
         cors: {
-            origin: '*',
+            origin: "*",
             credentials: true,
         },
     },
 });
+
+export const graphqlHandler = (event: any, context: any, callback: any) => {
+    console.log("Request event", event);
+    console.log("Request context: ", context);
+    let resBody, variables;
+    switch (true) {
+        case event.httpMethod === "GET" && event.path === establishmentPath:
+            const query = `
+                query GetEstablishment($getEstablishmentId: String!) {
+                    getEstablishment(id: $getEstablishmentId) {
+                        id
+                        name
+                    }
+                }
+                `;
+            variables = {
+                getEstablishmentId: event.queryStringParameters.id,
+            };
+            resBody = JSON.stringify({
+                query,
+                variables,
+            });
+            break;
+        case event.httpMethod === "POST" && event.path === establishmentPath:
+            const mutation = `
+                mutation CreateEstablishment($createEstablishmentId: String!, $name: String!) {
+                    createEstablishment(id: $createEstablishmentId, name: $name) {
+                        id
+                        name
+                    }
+                }`;
+            const body = JSON.parse(event.body);
+            variables = {
+                createEstablishmentId: body.id,
+                name: body.name,
+            };
+            resBody = JSON.stringify({
+                query: mutation,
+                variables,
+            });
+            break;
+        default:
+            break;
+    };
+
+    const newEvent = Object.assign(event, {
+        body: resBody,
+        requestContext: context,
+        resource: "/graphql",
+        path: "/graphql",
+        httpMethod: "POST",
+        headers: {
+            "Accept": "*/*",
+            "content-type": "application/json",
+        },
+    });
+
+    return apolloHandler(newEvent, context, callback);
+};
